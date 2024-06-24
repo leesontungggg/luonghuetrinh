9aa8d630b5fbf2ebda0112a0db3d8856ccc17e22
Trinhshu <66086251+Trinhshu@users.noreply.github.com>
2024-06-24 20:01:09 +0700

    bio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -147,8 +147,8 @@ export default function Home() {
           followed by her Master's in Multimedia Composition at Hochschule für Musik & Theater Hamburg. 
           <br />
           <br />
-          LUONG's artistic interests span traditional and experimental music elements, alongside visual arts and scenography, often creating site-specific artworks. Beyond composing, she is an adept improviser 
-          and has received commissions for exhibitions, contemporary dance projects, animation, experimental films, video arts, and live performances across Asia, Europe, Australia, Canada, North America, and Africa.
+          LUONG's artistic interests span traditional and experimental music elements, alongside visual arts and scenography, often creating site-specific artworks. Beyond composing, she is an adept improviser. 
+          She has received commissions for exhibitions, contemporary dance projects, animation, experimental films, video arts, and live performances across Asia, Europe, Australia, Canada, North America, and Africa.
           <br />
           <br />
           ------------------
@@ -157,19 +157,16 @@ export default function Home() {
           <br />
           Beginning her musical journey at the age of 5, LUONG Hue Trinh entered the Vietnam National Academy of Music in 1998 at the age of 13. 
           Over the next 12 years of dedicated study, she earned her Bachelor’s degree in Jazz Keyboard in 2010, guided by Vietnamese composer Nguyen Xuan Son (SonX). 
-          Since then, her focus has shifted to electroacoustic music.
-          <br />
-          <br />
-          From 2015 to 2018, LUONG was awarded a full German Academic Exchange Service (DAAD) scholarship for the New Techniques in Composition program, 
+          Since then, her focus has shifted to electroacoustic music. From 2015 to 2018, LUONG was awarded a full German Academic Exchange Service (DAAD) scholarship for the New Techniques in Composition program, 
           followed by her Master's in Multimedia Composition under the tutelage of Prof. Dr. Georg Hajdu, Prof. Elmar Lampson, and Alexander Schubert at Hochschule für Musik & Theater Hamburg.
           <br />
           <br />
           LUONG's artistic pursuits extend to exploring traditional and experimental elements in music and visuals, scenography, and creating site-specific artworks. 
-          Alongside composing, she excels as an improviser, receiving commissions for exhibitions, contemporary dance projects, animation, experimental films, video art, installations, painting performance and live performances...
+          She has received commissions for exhibitions, contemporary dance projects, animation, experimental films, video art, installations, painting performance and more...
           <br />
           <br />
           Her debut album "Illusions" in 2016 garnered acclaim and was recognized as one of the Best of 2016 – Albums of The Year by Avant Music News, San Francisco, USA. 
-          In 2019, Hanoi Grapevine Finest named her the Finest Artist of the year, marking her significant contribution to Vietnam's art and culture scene. 
+          In 2019, Hanoi Grapevine - the first and only bi-lingual site for art and cultural events in Vietnam, named her the 'Finest Artist of the Year', marking her significant contribution to Vietnam's art and culture scene. 
           She was also awarded a scholarship in the Réciprocité program of Villa Saigon-Institut Français Vietnam for a 3-month residency at the Cité Internationale des Arts Paris in 2020. 
           The same year, she was honored as one of 14 successful DAAD Vietnam alumni since 1985, featured in a publication commemorating 45 years of diplomatic relations between Germany and Vietnam.
           <br />
