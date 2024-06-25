3153a0eafa7e09297537912359ef0a13571f1de7
Trinhshu <66086251+Trinhshu@users.noreply.github.com>
2024-06-25 16:37:59 +0700

    media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1480,7 +1480,7 @@ export default function Home() {
           <li className="list-disc">
             <a
               className="underline cursor-pointer"
-              href="https://baohaiduong.vn/bat-chap-khoang-cach-nu-nghe-si-tu-4-quoc-gia-cung-bay-tiec-am-thanh-185319.html"
+              href="https://thethaovanhoa.vn/luong-hue-trinh-neu-khong-di-am-nhac-cua-toi-se-nham-chan-20210715081038238.htm"
             >
               Lương Huệ Trinh: Nếu không đi, âm nhạc của tôi sẽ nhàm chán!
             </a> - Thể thao Văn hóa/Culture & Sport Newspaper (VIET){" "}
@@ -1488,10 +1488,18 @@ export default function Home() {
           <li className="list-disc">
             <a
               className="underline cursor-pointer"
-              href="https://thethaovanhoa.vn/luong-hue-trinh-neu-khong-di-am-nhac-cua-toi-se-nham-chan-20210715081038238.htm"
+              href="https://www.vietnamplus.vn/bat-chap-khoang-cach-nu-nghe-sy-tu-4-quoc-gia-cung-bay-tiec-am-thanh-post727060.vnp"
             >
               Lương Huệ Trinh: Bất chấp khoảng cách, nữ nghệ sĩ từ 4 quốc gia cùng bày tiệc âm thanh
-            </a> - Hải Dương province Newspaper (VIET){" "}
+            </a> - Vietnamplus Newspaper (VIET/ENG){" "}
+          </li>
+          <li className="list-disc">
+            <a
+              className="underline cursor-pointer"
+              href="https://www.facebook.com/photo/?fbid=10159465697754313&set=a.10151546005599313"
+            >
+             "Tôi thích kết hợp yếu tố hiện đại với nghệ thuật truyền thống"
+            </a> - Lao động/Labor Newspaper (VIET){" "}
           </li>
           <li className="list-disc">
             <a
@@ -1504,10 +1512,42 @@ export default function Home() {
           <li className="list-disc">
             <a
               className="underline cursor-pointer"
+              href="https://www.vietnamplus.vn/lan-dau-tien-nhac-truyen-thong-viet-nam-hoa-cung-vu-dieu-ghana-post715943.vnp"
+            >
+              Lần đầu tiên nhạc truyền thống Việt Nam hòa quyện cùng vũ điệu Ghana 
+            </a>- Vietnamplus (VIET/ENG){" "}
+          </li>
+          <li className="list-disc">
+            <a
+              className="underline cursor-pointer"
               href="https://vietnamnews.vn/sunday/features/569873/emerging-musician-goes-her-own-way.html"
             >
               Emerging musician goes her own way 
             </a>- Việt Nam News (ENG){" "}
+          </li>
+          <li className="list-disc">
+            <a
+              className="underline cursor-pointer"
+              href="https://daidoanket.vn/nha-soan-nhac-luong-hue-trinh-nhac-moi-tiep-can-nguoi-nghe-theo-cach-tu-nhien-nhat-10143255.html"
+            >
+              Nhà soạn nhạc Lương Huệ Trinh: Nhạc Mới tiếp cận người nghe theo cách tự nhiên nhất
+            </a> - Đại Đoàn Kết Newspaper (VIET){" "}
+          </li>
+          <li className="list-disc">
+            <a
+              className="underline cursor-pointer"
+              href="https://www.phapfr.vn/am-nhac-nghe-thuat-bieu-dien/2019/12/05/hoa-nhac-lettres-de-ha-noi-thuong-nho-mot-thoi-xa-xua/"
+            >
+              Hòa nhạc "Lettres de Hanoï", thương nhớ một thời xa xưa
+            </a> - Phápfr.vn / Tạp chí Thương Gia - Luxury Inside (VIET){" "}
+          </li>
+          <li className="list-disc">
+            <a
+              className="underline cursor-pointer"
+              href="https://laodong.vn/van-hoa-giai-tri/thu-ha-noi-goc-nhin-am-nhac-tu-cam-hung-cac-buc-thu-xua-768602.ldo"
+            >
+              "Thư Hà Nội" - Góc nhìn âm nhạc từ các bức thư xưa
+            </a>- Lao động/Labor Newspaper (VIET){" "}
           </li>
           <li className="list-disc">
             <a
@@ -1540,6 +1580,14 @@ export default function Home() {
             >
               The musical journey of Luong Hue Trinh / Hành trình âm nhạc của Lương Huệ Trinh 
             </a>- Hanoi Grapevine (ENG/VIET){" "}
+          </li>
+          <li className="list-disc">
+            <a
+              className="underline cursor-pointer"
+              href="https://ttdn.vn/hoi-nhap-quoc-te/am-nhac-viet-nam-gay-choang-ngop-tai-le-hoi-am-nhac-punkt-1271"
+            >
+              Âm nhạc Việt Nam gây choáng ngợp tại Lễ hội âm nhạc PUNKT - Na Uy
+            </a> - Trang thông tin đối ngoại (VIET){" "}
           </li>
           <li className="list-disc">
             <a
