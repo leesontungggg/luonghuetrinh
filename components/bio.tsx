3573d0b9cc48bb8e8fc513b3195dd34986bb83ea
Trinhshu <66086251+Trinhshu@users.noreply.github.com>
2024-09-20 09:18:21 +0200

    add

--- components/bio.tsx
+++ components/bio.tsx
@@ -7,12 +7,12 @@ const Bio = () => {
       <p className="mt-8 text-[#777777]">
         Brief:
         <br />
-        <b>LUONG Hue Trinh</b> is a dynamic electroacoustic and multimedia composer and performer. 
+        <b>LƯƠNG Huệ Trinh</b> is a dynamic electroacoustic and multimedia composer and performer. 
         She initially studied Jazz Keyboard at the Vietnam National Academy of Music, and later, has focused on electroacoustic music. 
         She received a prestigious full scholarship from the German Academic Exchange Service (DAAD) to study New Techniques in Composition, 
         followed by a Master's in Multimedia Composition at Hochschule für Musik & Theater Hamburg.
         Her artistic profile blends traditional and experimental music, visuals, scenography, and site-specific performances. 
-        LUONG has collaborated with artists from diverse fields, including contemporary dance, 
+        LƯƠNG has collaborated with artists from diverse fields, including contemporary dance, 
         animation, experimental film, video art, poetry, drama, painting performance and installation etc. 
         She has showcased her work across Asia, Europe, Oceania, North America, and Africa.
         <br />
@@ -21,9 +21,9 @@ const Bio = () => {
         <br />
         Full:
         <br />
-        </b>LUONG Hue Trinh</b> is a dynamic electroacoustic and multimedia composer and performer. Starting her musical journey at age 5, she entered the Vietnam National Academy of Music at 13. 
-        She earned a Bachelor’s in Jazz Keyboard in 2010. She then focused on electroacoustic music, studying with Nguyen Xuan Son (SonX). 
-        From 2015 to 2018, LUONG received a full DAAD scholarship for New Techniques in Composition, 
+        <b>LƯƠNG Huệ Trinh</b> is a dynamic electroacoustic and multimedia composer and performer. Starting her musical journey at age 5, she entered the Vietnam National Academy of Music at 13. 
+        She earned a Bachelor’s in Jazz Keyboard in 2010. She then focused on electroacoustic music, studying with Nguyễn Xuân Sơn (SonX). 
+        From 2015 to 2018, LƯƠNG received a full DAAD scholarship for New Techniques in Composition, 
         followed by a Master's in Multimedia Composition at Hochschule für Musik & Theater Hamburg 
         under <a href="http://www.georghajdu.de"> <u>Prof. Dr. Georg Hajdu</u></a>, <a href="http://www.elmar-lampson.de/"> <u>Prof. Elmar Lampson</u></a>, 
         and <a href="http://www.alexanderschubert.net"> <u>Alexander Schubert</u></a>.
@@ -39,7 +39,7 @@ const Bio = () => {
         {isSeeMore && (
           <>
             <br />
-            LUONG's artistic pursuits extend to exploring traditional and
+            LƯƠNG's artistic pursuits extend to exploring traditional and
             experimental elements in music and visuals, scenography, and
             creating site-specific artworks. She has collaborated with artists from diverse fields, including contemporary dance, 
             animation, experimental film, video art, poetry, drama, painting performance and installation ...
@@ -51,7 +51,7 @@ const Bio = () => {
             and was highlighted as one of 14 notable DAAD Vietnam alumni (1985 - 2020) in a publication celebrating 45 years of German-Vietnamese relations.
             <br />
             <br />
-            LUONG's music has been featured internationally, including Artist of the Month on StillStream Radio (USA, 2013), 
+            LƯƠNG's music has been featured internationally, including Artist of the Month on StillStream Radio (USA, 2013), 
             London’s Gwaith Sŵn (UK), and NRK (Norway, 2014, 2015), Yfm and Mx24TV (Ghana, 2022, 2023). 
             She performed at the Mikromusik Festival (2018) organized by Berliner Künstlerprogramm des DAAD and Humboldt Forum Foundation, 
             and broadcast on Deutschlandfunk Kultur, Germany.
