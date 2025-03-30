755f2a8808c1cbf0e93df31b8eb695724fc49972
Luong Hue Trinh <luonghuetrinh@Luongs-Macbook-Pro.local>
2025-03-30 10:22:43 +0200

    e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const lobster = Lobster({
 
 const alegreya = Alegreya({
   subsets: ["latin"],
-  weight: ["500", "800", "900"],
+  weight: ["400", "700", "900"],
 });
 
 export const metadata: Metadata = {
