6ff61917332cd6040d6305ffe3a8ff735f7d1e97
Luong Hue Trinh <luonghuetrinh@Luongs-Macbook-Pro.local>
2025-04-17 21:06:31 +0200

    z

--- components/bio.tsx
+++ components/bio.tsx
@@ -4,7 +4,7 @@ const Bio = () => {
   const [isSeeMore, setIsSeeMore] = useState(false);
   return (
     <div className="h-fit w-full flex justify-center items-center bg-white py-8 lg:py-32 px-8 lg:px-72 flex-col">
-      <p className="mt-8 text-[#777777] text-xl">
+      <p className="mt-8 text-[#777777] text-lg">
         Brief:
         <br />
         <b>
